d285e6adc87fdc5900ca1949b2cee10cdf347181
Mitch Rose <mitch.rose@yale.edu>
2015-10-09 02:41:33 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/kind-lemur', function(request, response){
+    response.send('kind-lemur');
+});
+
 app.listen(process.env.PORT || 4000);
